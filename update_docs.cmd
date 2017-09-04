9098ef367f258122c2b4a3b6827b1b7be4d8f225
Alexander Tretyak <alextretyak@users.noreply.github.com>
2017-09-04 11:41:22 +1000

    2

---
+++ update_docs.cmd
@@ -0,0 +1,2 @@
+python ..\pqmarkup\pqmarkup.py --output-html-document ..\pqmarkup\docs\en\syntax.pq.txt -f index.html
+python ..\pqmarkup\pqmarkup.py --output-html-document ..\pqmarkup\docs\ru\syntax.pq.txt -f ru\index.html
