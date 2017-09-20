fef9ac93d726ce541a9d3d9cc781565df39aaa46
Alexander Tretyak <alextretyak@users.noreply.github.com>
2017-09-20 19:28:15 +1000

    Add main page

--- update_docs.cmd
+++ update_docs.cmd
@@ -1,2 +1,4 @@
+python ..\pqmarkup\pqmarkup.py --output-html-document .pq -f index.html
+python ..\pqmarkup\pqmarkup.py --output-html-document ru.pq -f ru\index.html
 python ..\pqmarkup\pqmarkup.py --output-html-document ..\pqmarkup\docs\en\syntax.pq.txt -f syntax\index.html
-python ..\pqmarkup\pqmarkup.py --output-html-document ..\pqmarkup\docs\ru\syntax.pq.txt -f syntax\ru\index.html
+python ..\pqmarkup\pqmarkup.py --output-html-document ..\pqmarkup\docs\ru\syntax.pq.txt -f ru\syntax\index.html
