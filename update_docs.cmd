37a6e0626746289420997311fefd26f7975cd77c
Alexander Tretyak <alextretyak@users.noreply.github.com>
2017-10-15 20:56:26 +1000

    +'‘@if not %ERRORLEVEL% == 0 pause ’' Чтобы update_docs.cmd не закрывался молча в случае ошибки [-правда вызов update_docs.cmd из start_commit_hook всё равно толком не работает :():-].

--- update_docs.cmd
+++ update_docs.cmd
@@ -1,4 +1,8 @@
 python ..\pqmarkup\pqmarkup.py --output-html-document .pq -f index.html
+@if not %ERRORLEVEL% == 0 pause
 python ..\pqmarkup\pqmarkup.py --output-html-document ru.pq -f ru\index.html
+@if not %ERRORLEVEL% == 0 pause
 python ..\pqmarkup\pqmarkup.py --output-html-document ..\pqmarkup\docs\en\syntax.pq.txt -f syntax\index.html
+@if not %ERRORLEVEL% == 0 pause
 python ..\pqmarkup\pqmarkup.py --output-html-document ..\pqmarkup\docs\ru\syntax.pq.txt -f ru\syntax\index.html
+@if not %ERRORLEVEL% == 0 pause
